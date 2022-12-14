3cd1a62732a85e5668c240e55d005ca09d7385e8
Cameron Montgomery <jc.montgomery0102@gmail.com>
2022-12-13 17:49:17 -0700

    Create useBreedList hook to get and cache breeds related to the selected animal

--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Pet from './Pet';
+import useBreedList from '../hooks/useBreedList';
 const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
 
 const SearchParams = () => {
@@ -7,10 +8,11 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState('');
   const [breed, setBreed] = useState('');
   const [pets, setPets] = useState([]);
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const requestPets = async () => {
@@ -18,7 +20,6 @@ const SearchParams = () => {
       `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
     const json = await res.json();
-    console.log(json.pets);
     setPets(json.pets);
   };
 
