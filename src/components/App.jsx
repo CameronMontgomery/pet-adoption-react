1eee979c4e3c86156ee54970d6b97970ad40bfc9
Cameron Montgomery <jc.montgomery0102@gmail.com>
2022-12-28 15:38:59 -0700

    Added prettier-plugin-tailwindcss dep and changed prettier config to be js file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import SearchParams from './SearchParams';
-import Details from './Details';
-import AdoptedPetContext from './AdoptedPetContext';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+import AdoptedPetContext from "./AdoptedPetContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,13 +20,13 @@ const App = () => {
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <AdoptedPetContext.Provider value={adoptedPet}>
-          <header>
-            <Link to='/'>Adopt Me!</Link>
+          <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center ">
+            <Link to="/">Adopt Me!</Link>
           </header>
           <div>
             <Routes>
-              <Route path='/details/:id' element={<Details />} />
-              <Route path='/' element={<SearchParams />} />
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
             </Routes>
           </div>
         </AdoptedPetContext.Provider>
@@ -36,6 +35,4 @@ const App = () => {
   );
 };
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+export default App;
