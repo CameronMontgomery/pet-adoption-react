f083685e88c18ad0db1d05677883e80d7e96f2b7
Cameron Montgomery <jc.montgomery0102@gmail.com>
2022-12-29 13:14:16 -0700

    Add and configure prettier tailwind plugin; Add styling to App header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
-import AdoptedPetContext from "./AdoptedPetContext";
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import SearchParams from './SearchParams';
+import Details from './Details';
+import AdoptedPetContext from './AdoptedPetContext';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,13 +20,15 @@ const App = () => {
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <AdoptedPetContext.Provider value={adoptedPet}>
-          <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center ">
-            <Link to="/">Adopt Me!</Link>
+          <header className='mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center'>
+            <Link className='text-6xl text-white hover:text-gray-200' to='/'>
+              Adopt Me!
+            </Link>
           </header>
           <div>
             <Routes>
-              <Route path="/details/:id" element={<Details />} />
-              <Route path="/" element={<SearchParams />} />
+              <Route path='/details/:id' element={<Details />} />
+              <Route path='/' element={<SearchParams />} />
             </Routes>
           </div>
         </AdoptedPetContext.Provider>
