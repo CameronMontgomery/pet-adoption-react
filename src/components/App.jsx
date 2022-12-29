2140de953556c7f9cc954b900e884062cc4b87ff
Cameron Montgomery <jc.montgomery0102@gmail.com>
2022-12-29 15:02:16 -0700

    Add tailwind classes to App and SearchParams

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <AdoptedPetContext.Provider value={adoptedPet}>
-          <header className='mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center'>
+          <header className='mb-10 w-full border-r-2 bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center'>
             <Link className='text-6xl text-white hover:text-gray-200' to='/'>
               Adopt Me!
             </Link>
